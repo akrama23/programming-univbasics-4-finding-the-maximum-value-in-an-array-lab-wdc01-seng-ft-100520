ae7c96868bad2c445304d072947e39322e279d4d
akam alam <arundelfootball23@gmail.com>
2020-09-24 06:23:09 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -2,7 +2,7 @@ def find_max_value(array)
   array = [1,2,1,3,4,3,5,4,3,2,1]
   highest_array = nil 
 count = 0 
- while count < array.max do 
+ while count < array.length do 
    array[count]
    highest_array= array[count] 
    count +=1 
